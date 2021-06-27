9ac0a20577358bea98018b4149204072114e2d78
Arkadiusz Milewski <asmilewski@gmail.com>
2021-06-27 13:02:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lyoyoyoyoyoyo
+          
         </a>
       </header>
     </div>
