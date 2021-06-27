7b0bcc6e118cf9d6f7b970482d1a18754cd170d7
Arkadiusz Milewski <asmilewski@gmail.com>
2021-06-27 14:42:45 +0200

    header.js connected to router

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,16 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 
 
 const App = () => {
   return (
-    <div>
+    <div className='ui container'>
       <BrowserRouter>
         <div>
+          <Header />    
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
           <Route path="/streams/edit" component={StreamEdit} />
