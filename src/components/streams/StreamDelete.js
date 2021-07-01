c3e30ad6db2da38a486df71ccbeb8e0c8c52a236
Arkadiusz Milewski <asmilewski@gmail.com>
2021-07-01 17:22:54 +0200

    Field component implemented for the first time

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const StreamDelete = () => {
-  return <div>Stream Delete</div>
+  return <div>Stream Delete</div>;
 };
 
-export default StreamDelete;                  
+export default StreamDelete;
