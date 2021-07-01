4f0dd920d2ae425ada36be20d359c3851f258053
Arkadiusz Milewski <asmilewski@gmail.com>
2021-07-01 16:51:50 +0200

    recording the user's ID done

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,45 +1,53 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { signOut, signIn } from "../actions";
 
 class GoogleAuth extends React.Component {
-state = { isSignedIn: null }
-
   componentDidMount() {
-    window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId: '314592811731-ut0sr7uojbf7kdt0nga5j7uctupc818e.apps.googleusercontent.com',
-        scope: 'email'
-      }).then(() => {
-        this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
-        this.auth.isSignedIn.listen(this.onAuthChange)
-      });
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "314592811731-ut0sr7uojbf7kdt0nga5j7uctupc818e.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange(this.auth.isSignedIn.get());
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
-  
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId());
+    } else {
+      this.props.signOut();
+    }
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
-  }
+  };
 
-  onSignOut= () => {
+  onSignOutClick = () => {
     this.auth.signOut();
-  }
+  };
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
-      return null 
-    } else if (this.state.isSignedIn) { 
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
       return (
-        <button onClick={ this.onSignOut} className='ui red google button'>
-          <i className='google icon' />
-            Sign Out
-        </button> 
-      )} else {
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
       return (
-        <button onClick={ this.onSignIn} className='ui red google button'>
-          <i className='google icon' />
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
           Sign In
         </button>
       );
@@ -47,8 +55,12 @@ state = { isSignedIn: null }
   }
 
   render() {
-    return <div>{this.renderAuthButton()}</div>
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
-export default GoogleAuth;
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
-import App from './components/App';
+import App from "./components/App";
+import reducers from "./reducers";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDom.render(
-  <App />,
-  document.querySelector('#root')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
