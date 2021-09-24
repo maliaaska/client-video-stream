72a542a0ef3f45fc0aebf82f5922e7187b0a1d90
Arkadiusz Milewski <asmilewski@gmail.com>
2021-09-24 17:28:18 +0200

    editing the stream works properly. issue with the Edit/delete buttons disappereance

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -9,9 +9,9 @@ class StreamEdit extends React.Component {
     this.props.fetchStream(this.props.match.params.id);
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
