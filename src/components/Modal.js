44b4171b349e9bf535353371c63d552bcd3a1766
Arkadiusz Milewski <asmilewski@gmail.com>
2021-09-26 15:11:10 +0200

    redirecting with the Link to the delete modul when click delete button in the StreamList component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div
-      onClick={() => history.push("/")}
-      className="ui dimmer modals visible active"
-    >
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div
         onClick={(e) => e.stopPropagation()} //e.stopPropagation prevents of bubble up the event handler to the parent( in this case history.push)
         className="ui standard modal visible active"
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Modal from "../Modal";
-
+import history from "../../history";
 const StreamDelete = () => {
   const actions = (
     <React.Fragment>
@@ -15,6 +15,7 @@ const StreamDelete = () => {
         title="Delete Stream"
         content="Are you sure you want to delete this stream?"
         actions={actions}
+        onDismiss={() => history.push("/")}
       />
     </div>
   );
