cbc97eaca3d41071fc77b7b1ecf1fe8b3c922875
Arkadiusz Milewski <asmilewski@gmail.com>
2021-09-28 18:37:36 +0200

    streaming properly but with the delay

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -20,6 +20,11 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  componentWillUnmount() {
+    console.log("i was unmounted");
+    this.player.destroy();
+  }
+
   buildPlayer() {
     if (this.player || !this.props.stream) {
       return;
